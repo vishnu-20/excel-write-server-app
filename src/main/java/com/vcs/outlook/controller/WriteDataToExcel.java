26bfc571b6369a547fb7414ad317bf066697854b
Vishnu <vishnulaleng@gmail.com>
2024-05-31 20:09:13 +0530

    commit changes

--- src/main/java/com/vcs/outlook/controller/WriteDataToExcel.java
+++ src/main/java/com/vcs/outlook/controller/WriteDataToExcel.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -30,94 +34,104 @@ public class WriteDataToExcel {
 	@Autowired
 	private ResourceLoader resourceLoader;
 
-	@PostConstruct
-	public void writeToW() throws Exception {
-		Resource resource = resourceLoader.getResource("classpath:Book1.xlsx");
-
-		String sheetName = "InstrumentDetails";
-		String filStr = resource.getURI().toURL().getPath();
-//		String filString = resource.getURI().toURL().getPath().replace("/","\\");
-		String filString = "C:\\Users\\vishn\\Downloads\\openoutlook\\openoutlook\\target\\classes\\Book1.xlsx";
-
-
-		List<InstrumentDetails> instrumentList = new ArrayList<>();
-		instrumentList.add(new InstrumentDetails(5027, "Guitar", "Vishnu", 299.99, LocalDate.of(2024, 1, 1)));
-		instrumentList.add(new InstrumentDetails(5028, "Piano", "lal", 499.99, LocalDate.of(2024, 2, 1)));
-		instrumentList.add(new InstrumentDetails(5029, "Flute", "Wind", 149.99, LocalDate.of(2024, 3, 1)));
-
-		Map<String, InstrumentDetails> studentData = instrumentList.stream()
-				.collect(Collectors.toMap(instrument -> String.valueOf(instrument.getIndexId()),
-						instrument -> instrument, (existing, replacement) -> existing, // Handle duplicate keys
-						TreeMap::new));
-		try {
-			File file = new File(filString);
-
-			XSSFWorkbook workbook;
-			Sheet sheet;
-
-			if (file.exists()) {
-				try (FileInputStream fis = new FileInputStream(file)) {
-					workbook = new XSSFWorkbook(fis);
-					sheet = workbook.getSheet(sheetName);
-					if (sheet == null) {
-						sheet = workbook.createSheet(sheetName);
-					} else {
-						// Clear the sheet
-						for (int i = sheet.getLastRowNum(); i > 0; i--) {
-							sheet.removeRow(sheet.getRow(i));
-						}
-					}
-				}
-			} else {
-				workbook = new XSSFWorkbook();
-				sheet = workbook.createSheet(sheetName);
-			}
-
-			// Create a CellStyle for formatting dates
-			CreationHelper createHelper = workbook.getCreationHelper();
-			CellStyle dateCellStyle = workbook.createCellStyle();
-			dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("yyyy-mm-dd"));
-
-			int rowid = 1;
-
-			Set<String> keyid = studentData.keySet();
-			for (String key : keyid) {
-				Row row = sheet.createRow(rowid++);
-				InstrumentDetails objectArr = studentData.get(key);
-				int cellid = 0;
-
-				Cell cell = row.createCell(cellid++);
-				cell.setCellValue(objectArr.getIndexId());
-
-				cell = row.createCell(cellid++);
-				cell.setCellValue(objectArr.getName());
-
-				cell = row.createCell(cellid++);
-				cell.setCellValue(objectArr.getType());
-
-				cell = row.createCell(cellid++);
-				cell.setCellValue(objectArr.getPrice());
-
-				cell = row.createCell(cellid++);
-				LocalDate localDate = objectArr.getDate();
-				Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-				cell.setCellValue(date);
-				cell.setCellStyle(dateCellStyle); // Apply date format
-			}
-
-			// Auto-size columns to fit the contents
-			for (int i = 0; i < 5; i++) {
-				sheet.autoSizeColumn(i);
-			}
-
-			try (FileOutputStream out = new FileOutputStream(file)) {
-				workbook.write(out);
-			}
-			workbook.close();
-
-			System.out.println("Excel file written successfully.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	
+//	@PostConstruct
+	public void pub() throws Exception {
+        String sheetName = "InstrumentDetails";
+//		writeToW(sheetName);
+//		writeToW("InstrumentDetails1");
 	}
+
+	public void writeToW(String sheetName , List<InstrumentDetails> instrumentList) throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:Book1.xlsx");
+
+        // Copy the resource file to a temporary location
+//        File tempFile = resource.getFile();
+        
+        File tempFile = new File("src/main/resources/Book1.xlsx");
+
+
+
+
+
+        Map<String, InstrumentDetails> studentData = instrumentList.stream()
+                .collect(Collectors.toMap(instrument -> String.valueOf(instrument.getIndexId()),
+                        instrument -> instrument, (existing, replacement) -> existing, // Handle duplicate keys
+                        TreeMap::new));
+
+        try {
+            XSSFWorkbook workbook;
+            Sheet sheet;
+
+            try (FileInputStream fis = new FileInputStream(tempFile)) {
+                workbook = new XSSFWorkbook(fis);
+                sheet = workbook.getSheet(sheetName);
+                if (sheet == null) {
+                    sheet = workbook.createSheet(sheetName);
+                } else {
+                    // Clear the sheet
+                    for (int i = sheet.getLastRowNum(); i > 0; i--) {
+                        sheet.removeRow(sheet.getRow(i));
+                    }
+                }
+            }
+
+            // Create a CellStyle for formatting dates
+            CreationHelper createHelper = workbook.getCreationHelper();
+            CellStyle dateCellStyle = workbook.createCellStyle();
+            dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("yyyy-mm-dd"));
+
+            int rowid = 1;
+
+            Set<String> keyid = studentData.keySet();
+            for (String key : keyid) {
+                Row row = sheet.createRow(rowid++);
+                InstrumentDetails objectArr = studentData.get(key);
+                int cellid = 0;
+
+                Cell cell = row.createCell(cellid++);
+                cell.setCellValue(objectArr.getIndexId());
+
+                cell = row.createCell(cellid++);
+                cell.setCellValue(objectArr.getName());
+
+                cell = row.createCell(cellid++);
+                cell.setCellValue(objectArr.getType());
+
+                cell = row.createCell(cellid++);
+                cell.setCellValue(objectArr.getPrice());
+
+                cell = row.createCell(cellid++);
+                LocalDate localDate = objectArr.getDate();
+                Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                cell.setCellValue(date);
+                cell.setCellStyle(dateCellStyle); // Apply date format
+            }
+
+            // Auto-size columns to fit the contents
+            for (int i = 0; i < 5; i++) {
+                sheet.autoSizeColumn(i);
+            }
+            File originalFile = new File("src/main/resources/Book1.xlsx");
+
+            try (FileOutputStream out = new FileOutputStream(originalFile)) {
+                workbook.write(out);
+                out.close();
+                
+            }
+            workbook.close();
+
+            System.out.println("Excel file written successfully.");
+
+//            // Replace the original file in the resources directory
+//            Files.copy(tempFile.toPath(), originalFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Clean up the temporary file if necessary
+            tempFile.deleteOnExit();
+        }
+    }
+
 }
--- src/test/java/com/vcs/outlook/OpenoutlookApplicationTests.java
+++ src/test/java/com/vcs/outlook/OpenoutlookApplicationTests.java
@@ -1,13 +1,40 @@
 package com.vcs.outlook;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vcs.outlook.controller.InstrumentDetails;
+import com.vcs.outlook.controller.WriteDataToExcel;
 
 @SpringBootTest
 class OpenoutlookApplicationTests {
+	
+	@Autowired
+	WriteDataToExcel dataToExcel;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		
+
+        List<InstrumentDetails> instrumentList = new ArrayList<>();
+        instrumentList.add(new InstrumentDetails(5027, "Guitar", "Vishnu", 299.99, LocalDate.of(2024, 1, 1)));
+        instrumentList.add(new InstrumentDetails(5028, "Piano", "lal", 499.99, LocalDate.of(2024, 2, 1)));
+        instrumentList.add(new InstrumentDetails(5029, "Flute", "Wind", 149.99, LocalDate.of(2024, 3, 1)));
+
+        AssertionFailedError exception = Assertions.assertThrows(AssertionFailedError.class, () -> {
+            Assertions.assertEquals(1, 2, "message");
+        });
+        
+        String string = exception.getMessage();
+		dataToExcel.writeToW("sheeNew", instrumentList);
+
 	}
 
 }
